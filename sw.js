a6c8e94882aa9986d9c98259b729c55154416d3a
iwaryoweb <iwaryoweb@gmail.com>
2026-08-22 12:35:37 +0900

    v1.2: HTTPエラーの理由表示・復帰時の再送・SW更新の自動リロード（SWキャッシュv3）

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'nikki-pen-v2';
+const CACHE = 'nikki-pen-v3';
 const SHELL = ['./', './index.html', './manifest.webmanifest', './apple-touch-icon.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
